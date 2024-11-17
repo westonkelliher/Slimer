5b67282c01fd333473482e181babeb7aaa90aa19
Weston <westonkelliher@gmail.com>
2024-11-16 18:23:11 -0800

    character walking and animated

--- player_guy.gd
+++ player_guy.gd
@@ -1,11 +1,18 @@
-extends RigidBody2D
+extends CharacterBody2D
 
 @export var SPEED := 300.0
-@export var ACC := 6000.0
+@export var ACC := 3000.0
+@export var SPRITE_SCALE := 3.0
+
+enum Direction {Up, Down, Left, Right}
+var direction := Direction.Down
 
 func _physics_process(delta: float) -> void:
 	handle_movement(delta)
+	apply_forces(delta)
 
+func apply_forces(delta: float) -> void:
+	position += velocity*delta
 
 func get_move_direction() -> Vector2:
 	var move_dir := Vector2.ZERO
@@ -22,11 +29,36 @@ func get_move_direction() -> Vector2:
 
 func handle_movement(delta: float) -> void:
 	var move_dir := get_move_direction()
-	var current_dir := linear_velocity.normalized()
-	# braking force
-	apply_force(-current_dir*ACC)
-	# running force
-	var remaining_speed := SPEED - linear_velocity.length()
-	apply_force(move_dir*(ACC + min(ACC, remaining_speed/delta)))
-	
-	
+	var current_dir := velocity.normalized()
+	velocity = velocity.move_toward(move_dir*SPEED, ACC*delta)
+	choose_animation(move_dir)
+
+func choose_animation(dir: Vector2) -> void:
+	if dir.x > 0:
+		direction = Direction.Right
+		$Sprite.play("walking_side")
+	elif dir.x < 0:
+		direction = Direction.Left
+		$Sprite.play("walking_side")
+	elif dir.y > 0:
+		direction = Direction.Down
+		$Sprite.play("walking_down")
+	elif dir.y < 0:
+		direction = Direction.Up
+		$Sprite.play("walking_up")
+	else:
+		if direction == Direction.Up:
+			$Sprite.play("resting_up")
+		if direction == Direction.Down:
+			$Sprite.play("resting_down")
+		else:
+			$Sprite.play("resting_side")
+			
+	#
+	if direction == Direction.Left:
+		$Sprite.scale.x = -SPRITE_SCALE
+	else:
+		$Sprite.scale.x = SPRITE_SCALE
+		
+		
+		
